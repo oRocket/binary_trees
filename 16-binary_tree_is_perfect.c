fd849df18dc135ad7297057ff08d21bd4a57f8e5
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-04 07:55:22 -0800

    Is perfect

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height
+ * of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ?
+				left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height, nodes, perfect_nodes;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	nodes = binary_tree_size(tree);
+	perfect_nodes = (1 << height) - 1;
+
+	return (nodes == perfect_nodes);
+}
