9f90663c862a8156bf1d48965a616a42800254ee
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-04 06:37:43 -0800

    fixed balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,5 +32,6 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+	return ((int)(binary_tree_height(tree->left) -
+				binary_tree_height(tree->right)));
 }
