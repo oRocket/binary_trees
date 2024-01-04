cfae224d67bcb9dcdad5572f8c47b6462c7c54c7
Albert Opoku-Twumasi <aotwum.at@outlook.com>
2024-01-04 07:59:11 -0800

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,42 +1,54 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_height - Measures the height
- * of a binary tree
- * @tree: Pointer to the root node of the tree
+ * tree_is_perfect - function that says if a tree is perfect or not
+ * it has to be the same quantity of levels in left as right, and also
+ * each node has to have 2 nodes or none
+ * @tree: tree to check
  *
- * Return: Height of the tree
+ * Return: 0 if is not a perfect or
+ * other number that is the level of height
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	int l = 0, r = 0;
 
-	if (tree == NULL)
+	if (tree->left && tree->right)
+	{
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return ((left_height > right_height ?
-				left_height : right_height) + 1);
+	}
+	else if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
-
 /**
- * binary_tree_is_perfect - Checks if a binary tree is perfect
- * @tree: Pointer to the root node of the tree to check
- *
- * Return: 1 if the tree is perfect, 0 otherwise
+ * binary_tree_is_perfect - perfect or not a tree
+ * @tree: tree to check
+ * Return: 1 is it is or 0 if not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height, nodes, perfect_nodes;
+	int result = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
-
-	height = binary_tree_height(tree);
-	nodes = binary_tree_size(tree);
-	perfect_nodes = (1 << height) - 1;
-
-	return (nodes == perfect_nodes);
+	}
+	else
+	{
+		result = tree_is_perfect(tree);
+		if (result != 0)
+		{
+			return (1);
+		}
+		return (0);
+	}
 }
